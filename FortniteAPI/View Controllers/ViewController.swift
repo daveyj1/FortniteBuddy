4142d8a6d3e2fe91eb752bded0a23198171a32c1
daveyj <daveyj@purdue.edu>
2018-03-20 13:06:16 -0400

    Added match type selector functionality

--- FortniteAPI/View Controllers/ViewController.swift
+++ FortniteAPI/View Controllers/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var killDeathLabel: UILabel!
     @IBOutlet weak var KillsPerMatchLabel: UILabel!
     @IBOutlet weak var ScorePerMatchLabel: UILabel!
-    
+    @IBOutlet weak var matchTypeSelector: UISegmentedControl!
     
     //var games:
     var username = ""
@@ -37,6 +37,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         getSoloStats()
+        //getDuoStats()
+        //getSquadStats()
+        //getStats()
     }
     
     func getSoloStats() {
@@ -142,6 +145,212 @@ class ViewController: UIViewController {
         task.resume()
     }
     
+    func getDuoStats() {
+        let urlString = URL(string: "https://api.fortnitetracker.com/v1/profile/pc/\(username)")
+        var req: URLRequest = URLRequest.init(url: urlString!)
+        req.setValue("7c57a9c5-6600-4e0f-a292-74a02cc1bcb6", forHTTPHeaderField: "TRN-Api-Key")
+        let task = URLSession.shared.dataTask(with: req) { (data, response, error) in
+            if error != nil {
+                print(error)
+            } else {
+                guard let usableData = data else {
+                    return
+                }
+                
+                guard let json = try! JSONSerialization.jsonObject(with: usableData, options: []) as? [String: Any] else {
+                    return
+                }
+                
+                if let error = json["error"] as? String {
+                    if error == "Player Not Found" {
+                        print("player not found")
+                        self.working = false
+                    }
+                }
+                
+                guard let soloJSON = json["stats"] as? [String: Any] else {
+                    return
+                }
+                guard let currP2 = soloJSON["curr_p10"] as? [String: Any] else {
+                    return
+                }
+                
+                //GETTING ALL THE VALUES FOR THE LABELS
+                //=====================================
+                if let user = json["epicUserHandle"]! as? String {
+                    self.username = user
+                }
+                
+                guard let wins = currP2["top1"] as? [String: Any] else {
+                    return
+                }
+                self.wins = wins["displayValue"]! as! String
+                
+                guard let avgPlay = currP2["avgTimePlayed"] as? [String: Any] else {
+                    return
+                }
+                self.survivalTime = avgPlay["displayValue"]! as! String
+                
+                guard let kills = currP2["kills"] as? [String: Any] else {
+                    return
+                }
+                self.kills = kills["displayValue"]! as! String
+                
+                guard let score = currP2["score"] as? [String: Any] else {
+                    return
+                }
+                self.score = score["displayValue"]! as! String
+                
+                guard let killDeath = currP2["kd"] as? [String: Any] else {
+                    return
+                }
+                self.killDeath = killDeath["displayValue"]! as! String
+                
+                guard let kpm = currP2["kpg"] as? [String: Any] else {
+                    return
+                }
+                self.kpm = kpm["displayValue"]! as! String
+                
+                guard let spm = currP2["scorePerMatch"] as? [String: Any] else {
+                    return
+                }
+                self.spm = spm["displayValue"]! as! String
+                //==================================
+                
+                /*
+                 for (key, _) in currP2 {
+                 print(key)
+                 }
+                 */
+            }
+            OperationQueue.main.addOperation {
+                if self.working {
+                    self.usernameLabel.text = self.username
+                    self.winsLabel.text = "Wins: \(self.wins)"
+                    self.killsLabel.text = "Kills: \(self.kills)"
+                    self.survivalTimeLabel.text = "Survival Time: \(self.survivalTime)"
+                    self.scoreLabel.text = "Score: \(self.score)"
+                    self.killDeathLabel.text = "Kill/Death: \(self.killDeath)"
+                    self.KillsPerMatchLabel.text = "Kills per Match: \(self.kpm)"
+                    self.ScorePerMatchLabel.text = "Score per Match: \(self.spm)"
+                } else {
+                    self.usernameLabel.text = "Player Not Found"
+                    self.winsLabel.text = "Wins: NA"
+                    self.killsLabel.text = "Kills: NA"
+                    self.survivalTimeLabel.text = "Survival Time: NA"
+                    self.scoreLabel.text = "Score: NA"
+                    self.killDeathLabel.text = "Kill/Death: NA"
+                    self.KillsPerMatchLabel.text = "Kills per Match: NA"
+                    self.ScorePerMatchLabel.text = "Score per Match: NA"
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    func getSquadStats() {
+        let urlString = URL(string: "https://api.fortnitetracker.com/v1/profile/pc/\(username)")
+        var req: URLRequest = URLRequest.init(url: urlString!)
+        req.setValue("7c57a9c5-6600-4e0f-a292-74a02cc1bcb6", forHTTPHeaderField: "TRN-Api-Key")
+        let task = URLSession.shared.dataTask(with: req) { (data, response, error) in
+            if error != nil {
+                print(error)
+            } else {
+                guard let usableData = data else {
+                    return
+                }
+                
+                guard let json = try! JSONSerialization.jsonObject(with: usableData, options: []) as? [String: Any] else {
+                    return
+                }
+                
+                if let error = json["error"] as? String {
+                    if error == "Player Not Found" {
+                        print("player not found")
+                        self.working = false
+                    }
+                }
+                
+                guard let soloJSON = json["stats"] as? [String: Any] else {
+                    return
+                }
+                guard let currP2 = soloJSON["curr_p9"] as? [String: Any] else {
+                    return
+                }
+                
+                //GETTING ALL THE VALUES FOR THE LABELS
+                //=====================================
+                if let user = json["epicUserHandle"]! as? String {
+                    self.username = user
+                }
+                
+                guard let wins = currP2["top1"] as? [String: Any] else {
+                    return
+                }
+                self.wins = wins["displayValue"]! as! String
+                
+                guard let avgPlay = currP2["avgTimePlayed"] as? [String: Any] else {
+                    return
+                }
+                self.survivalTime = avgPlay["displayValue"]! as! String
+                
+                guard let kills = currP2["kills"] as? [String: Any] else {
+                    return
+                }
+                self.kills = kills["displayValue"]! as! String
+                
+                guard let score = currP2["score"] as? [String: Any] else {
+                    return
+                }
+                self.score = score["displayValue"]! as! String
+                
+                guard let killDeath = currP2["kd"] as? [String: Any] else {
+                    return
+                }
+                self.killDeath = killDeath["displayValue"]! as! String
+                
+                guard let kpm = currP2["kpg"] as? [String: Any] else {
+                    return
+                }
+                self.kpm = kpm["displayValue"]! as! String
+                
+                guard let spm = currP2["scorePerMatch"] as? [String: Any] else {
+                    return
+                }
+                self.spm = spm["displayValue"]! as! String
+                //==================================
+                
+                /*
+                 for (key, _) in currP2 {
+                 print(key)
+                 }
+                 */
+            }
+            OperationQueue.main.addOperation {
+                if self.working {
+                    self.usernameLabel.text = self.username
+                    self.winsLabel.text = "Wins: \(self.wins)"
+                    self.killsLabel.text = "Kills: \(self.kills)"
+                    self.survivalTimeLabel.text = "Survival Time: \(self.survivalTime)"
+                    self.scoreLabel.text = "Score: \(self.score)"
+                    self.killDeathLabel.text = "Kill/Death: \(self.killDeath)"
+                    self.KillsPerMatchLabel.text = "Kills per Match: \(self.kpm)"
+                    self.ScorePerMatchLabel.text = "Score per Match: \(self.spm)"
+                } else {
+                    self.usernameLabel.text = "Player Not Found"
+                    self.winsLabel.text = "Wins: NA"
+                    self.killsLabel.text = "Kills: NA"
+                    self.survivalTimeLabel.text = "Survival Time: NA"
+                    self.scoreLabel.text = "Score: NA"
+                    self.killDeathLabel.text = "Kill/Death: NA"
+                    self.KillsPerMatchLabel.text = "Kills per Match: NA"
+                    self.ScorePerMatchLabel.text = "Score per Match: NA"
+                }
+            }
+        }
+        task.resume()
+    }
+    
     func getStats() {
         let urlString = URL(string: "https://api.fortnitetracker.com/v1/profile/pc/\(username)")
         var req: URLRequest = URLRequest.init(url: urlString!)
@@ -200,6 +409,20 @@ class ViewController: UIViewController {
         }
         task.resume()
     }
+    
+    @IBAction func selectMatchType(_ sender: UISegmentedControl) {
+        if matchTypeSelector.selectedSegmentIndex == 0 {
+            getSoloStats()
+            print("Solo Stats")
+        } else if matchTypeSelector.selectedSegmentIndex == 1 {
+            getDuoStats()
+            print("Duo Stats")
+        } else if matchTypeSelector.selectedSegmentIndex == 2 {
+            getSquadStats()
+            print("Squad Stats")
+        }
+    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
