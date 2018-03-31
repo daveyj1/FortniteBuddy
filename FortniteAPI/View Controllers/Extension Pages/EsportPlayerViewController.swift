20fcd6e1b4bdf7264c6a57c203ca6d29ceec06c3
daveyj <daveyj@purdue.edu>
2018-03-31 16:39:11 -0400

    Esports tab done

--- FortniteAPI/View Controllers/Extension Pages/EsportPlayerViewController.swift
+++ FortniteAPI/View Controllers/Extension Pages/EsportPlayerViewController.swift
@@ -11,12 +11,76 @@ import UIKit
 class EsportPlayerViewController: UIViewController {
     
     @IBOutlet weak var teamLabel: UILabel!
+    @IBOutlet weak var firstLabel: UIButton!
+    @IBOutlet weak var secondLabel: UIButton!
+    @IBOutlet weak var thirdLabel: UIButton!
+    @IBOutlet weak var fourthLabel: UIButton!
+    @IBOutlet weak var imageLabel: UIImageView!
     
+    var tsmPlayers = ["Hamlinz", "TSM_Myth", "TSM_CaMiLLS", "Daequan"]
+    var opticPlayers = ["H3CZ", "CouRageJD", "BigTymer8", "OpticScumpii"]
+    var fazePlayers = ["Faze SpaceLyon", "Faze Jaomock", "Faze Cloak", "ItsCizzorz"]
     var teamName: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         teamLabel.text = teamName
+        if teamLabel.text == "Optic Gaming" {
+            firstLabel.setTitle(opticPlayers[0], for: UIControlState.normal)
+            secondLabel.setTitle(opticPlayers[1], for: UIControlState.normal)
+            thirdLabel.setTitle(opticPlayers[2], for: UIControlState.normal)
+            fourthLabel.setTitle(opticPlayers[3], for: UIControlState.normal)
+            imageLabel.image = #imageLiteral(resourceName: "optic logo")
+        } else if teamLabel.text == "Faze Clan" {
+            firstLabel.setTitle(fazePlayers[0], for: UIControlState.normal)
+            secondLabel.setTitle(fazePlayers[1], for: UIControlState.normal)
+            thirdLabel.setTitle(fazePlayers[2], for: UIControlState.normal)
+            fourthLabel.setTitle(fazePlayers[3], for: UIControlState.normal)
+            imageLabel.image = #imageLiteral(resourceName: "Fazelogo")
+        } else {
+            firstLabel.setTitle(tsmPlayers[0], for: UIControlState.normal)
+            secondLabel.setTitle(tsmPlayers[1], for: UIControlState.normal)
+            thirdLabel.setTitle(tsmPlayers[2], for: UIControlState.normal)
+            fourthLabel.setTitle(tsmPlayers[3], for: UIControlState.normal)
+            imageLabel.image = #imageLiteral(resourceName: "TSMlogo")
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let dvc = segue.destination as! ViewController
+        if segue.identifier == "firstPlayerSegue" {
+            if teamLabel.text == "Optic Gaming" {
+                dvc.username = opticPlayers[0]
+            } else if teamLabel.text == "Faze Clan" {
+                dvc.username = fazePlayers[0]
+            } else {
+                dvc.username = tsmPlayers[0]
+            }
+        } else if segue.identifier == "secondPlayerSegue" {
+            if teamLabel.text == "Optic Gaming" {
+                dvc.username = opticPlayers[1]
+            } else if teamLabel.text == "Faze Clan" {
+                dvc.username = fazePlayers[1]
+            } else {
+                dvc.username = tsmPlayers[1]
+            }
+        } else if segue.identifier == "thirdPlayerSegue" {
+            if teamLabel.text == "Optic Gaming" {
+                dvc.username = opticPlayers[2]
+            } else if teamLabel.text == "Faze Clan" {
+                dvc.username = fazePlayers[2]
+            } else {
+                dvc.username = tsmPlayers[2]
+            }
+        } else if segue.identifier == "fourthPlayerSegue" {
+            if teamLabel.text == "Optic Gaming" {
+                dvc.username = opticPlayers[3]
+            } else if teamLabel.text == "Faze Clan" {
+                dvc.username = fazePlayers[3]
+            } else {
+                dvc.username = tsmPlayers[3]
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
